daa640fe73c4a770d98e9ff308426e774d347ad4
pritam priyadarshi <pritamcodes@users.noreply.huggingface.co>
2025-04-14 15:43:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ keyword_synonyms = {
     "finance": ["money", "loan", "subsidy", "pension", "support", "fund", "bank"],
     "employment": ["job", "work", "skill", "training", "internship"],
     "housing": ["home", "house", "shelter", "pradhan mantri awas", "pmay"],
+    "agriculture"
 }
 
 # Flatten synonyms for reverse lookup
